4f04fd4186addd881a0a72cabaa5dfe897872f50
fortuinp <pinda.fortuin@gmail.com>
2024-02-07 10:05:07 +0200

    add

--- addition.js
+++ addition.js
@@ -1,1 +1,14 @@
-console.log('add');
+// console.log('add')
+// module.exports
+
+// function addition(a,b) {
+//     return a+b
+// }
+// module.exports={
+//     addition
+// }
+
+ //use the one above when you want to export manny functions
+exports.addition=(a,b)=>{
+    return a+b
+}
