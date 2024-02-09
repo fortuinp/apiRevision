a47ab344f7626706fa7ea7bbe04918dc323aac82
fortuinp <pinda.fortuin@gmail.com>
2024-02-09 16:13:44 +0200

    last

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ http.createServer((req,res)=>{
 res.writeHead(200,{
     'Content-type':'application/json'
 })
-console.log(`
-URL:${req.url}
-HTTP method:${req.method}`);
+// console.log(`
+// URL:${req.url}
+// HTTP method:${req.method}`);
 
 
-}).listen(port)
+switch(req.url){
+    case '/':
+        res.end(JSON.stringify("you're home"))
+        break
+        case '/about':
+        res.end(JSON.stringify("About page"))   
+        break
+        default:
+            res.end(JSON.stringify("404 page"))   
+}
+
+}).listen(port,()=>{
+    console.log(`server is running on : http://localhost:${port}`);
+})
